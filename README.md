339d9e8192dc875c3f918074730d1b4f69e8f766
ryzent <thalalatha12@hotmail.com>
2021-10-03 22:02:41 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# Happy Hacktoberfest!
+What it is?
+> This is just simple script that print your name and text in dart language
+
+# How to join the club?
+- First go to https://hacktoberfest.digitalocean.com/
+- Make sure you've signed up with GitHub, so they will count your contribution from this repository
+- After registered, you can pull request this repository
+
+# How to pull request?
+- Fork this repo
+- Edit `bin\hacktoberfest.dart`
+- Insert `me("your name", "whatever text");` in last of `void contributors(){`
+- Example:
+```
+void contributors() {
+    me("Ryzntx", "Feel free to pull request");
+    
+    me("your name", "your wish/about"); 
+  }
+```
+- Go to main repository, then create a pull request
---
+++ bin/hacktoberfest.dart
@@ -0,0 +1,32 @@
+// Hacktoberfest 2021
+// To join the club, go to https://hacktoberfest.digitalocean.com/ and register yourself
+// Then you are ready to pull request with this repository
+
+void me(String name, about) {
+  print("${name}, ${about}");
+}
+
+void contributors() {
+  /*How to add your name:
+  insert same like below, in last of def contributors():
+  me("your name", "your wish");
+   
+  Example:
+   
+  void contributors() {
+    me("first contributor", "i like dart");
+    me("your contributor", "i like code"); <-- your example pull request
+  }
+  
+  More info check pull requests
+  */
+
+  me("Ryzntx", "Feel free to pull request");
+}
+
+void main(List<String> arguments) {
+  print('');
+  print('Happy Hacktoberfest 2021!');
+  print('Here is our contributor from worldwide: ');
+  contributors();
+}
